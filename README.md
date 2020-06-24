6d60f38187b973adbc43f384aee14e77cedb1e0a
Steven Marshall <SMarshall@chef.io>
2020-06-24 11:24:42 +0100

    [aws-cli] Various refactors to existing code
    Add inspec.lock to .gitignore
    Update README.md to latest binary format
    Update inspec to latest version
    
    Major changes to attributes.yml / controls
      Added lists of binaries to test / ignore
      Added tests to check the binaries exist and are executable
      Added tests to check the version command works
    
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The [AWS Command Line Interface](https://aws.amazon.com/cli/) (CLI) is a unified
 
 ## Maintainers
 
-The Habitat Maintainers humans@habitat.sh
+* The Core Planners: <chef-core-planners@chef.io>
 
 ## Type of Package
 
@@ -14,14 +14,71 @@ The Habitat Maintainers humans@habitat.sh
 
 ## Usage
 
-### Unix
-```bash
-hab pkg install core/aws-cli
-hab pkg exec core/aws-cli aws --version
-```
+### Use as Dependency
 
-### Windows
-```powershell
-hab pkg install core/aws-cli
-hab pkg exec core/aws-cli aws.cmd --version
+Binary packages can be set as runtime or build time dependencies. See [Defining your dependencies](https://www.habitat.sh/docs/developing-packages/developing-packages/#sts=Define%20Your%20Dependencies) for more information.
+
+To add core/aws-cli as a depdendency, you can add one of the following to your plan file.
+
+##### Buildtime Dependency
+
+> pkg_build_deps=(core/aws-cli)
+
+##### Runtime Depdendency
+
+> pkg_deps=(core/aws-cli)
+
+### Use as Tool
+
+#### Installation
+
+To install this plan, you should run the following commands to first install, and then link the binaries this plan creates.
+
+`hab pkg install core/aws-cli`
+
+> » Installing core/aws-cli
+>
+>☁ Determining latest version of core/aws-cli in the 'stable' channel
+>
+> ↓ Downloading core/aws-cli/1.18.25/20200319204505
+>
+> ☛ Verifying core/aws-cli/1.18.25/20200319204505
+>
+> ...
+>
+> ✓ Installed core/aws-cli/1.18.25/20200319204505
+>
+> ★ Install of core/aws-cli/1.18.25/20200319204505 complete with 3 new packages installed.
+
+`hab pkg binlink core/aws-cli`
+
+> » Binlinking aws.cmd from core/aws-cli into /bin
+>
+> ★ Binlinked aws.cmd from core/aws-cli/1.18.25/20200319204505 to /bin/aws.cmd
+
+#### Using an example binary
+You can now use the binary as normal:
+
+`/bin/aws --help` or `aws --help`
+
+```
+AWS()                                                                    AWS()
+
+
+
+NAME
+       aws -
+
+DESCRIPTION
+       The  AWS  Command  Line  Interface is a unified tool to manage your AWS
+       services.
+
+SYNOPSIS
+          aws [options] <command> <subcommand> [parameters]
+
+       Use aws command help for information on a  specific  command.  Use  aws
+       help  topics  to view a list of available help topics. The synopsis for
+       each command shows its parameters and their usage. Optional  parameters
+       are shown in square brackets.
+...
 ```
--- controls/aws_cli_works.rb
+++ controls/aws_cli_works.rb
@@ -3,6 +3,8 @@ title 'Tests to confirm AWS CLI works as expected'
 plan_name = input('plan_name', value: 'aws-cli')
 plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
 hab_path = input('hab_path', value: '/tmp/hab')
+version_check_allow_list = input('version_check_allow_list')
+version_check_block_list = input('version_check_block_list')
 
 control 'core-plans-aws-cli' do
   impact 1.0
@@ -11,10 +13,12 @@ control 'core-plans-aws-cli' do
   We first check that the AWS executable is present and then
   run the AWS CLI `--version` method to verify it is working.
   '
-  aws = command("#{hab_path} pkg path #{plan_ident}")
+  #aws = command("#{hab_path} pkg path #{plan_ident}")
+  aws = command("hab pkg path #{plan_ident}")
   describe aws do
-    its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+    its('exit_status') { should eq 0 }
   end
   aws = "#{aws.stdout.strip}/bin/aws"
 
@@ -25,8 +29,46 @@ control 'core-plans-aws-cli' do
   end
 
   describe command("#{aws} --version") do
-    its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
     its('stdout') { should match /aws-cli/ }
+    its('stderr') { should be_empty }
+    its('exit_status') { should eq 0 }
+  end
+end
+
+control 'core-plans-aws-binaries' do
+  impact 1.0
+  title 'Ensure binaries provided by aws-cli work as expected'
+  desc '
+  AWS CLI bundles many binaries as part of its .hart file.
+  In order to test this binaries we will first check that they exist and then
+  run the binaries to check their version to verify they are working.
+  '
+
+  #pkg_path = command("#{hab_path} pkg path #{plan_ident}")
+  pkg_path = command("hab pkg path #{plan_ident}")
+  describe pkg_path do
+    its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+    its('exit_status') { should eq 0 }
+  end
+  bin_directory = "#{pkg_path.stdout.strip}/bin/"
+  
+  # Arry of all binaries for existance check by union
+  binary_checks = version_check_allow_list | version_check_block_list
+  binary_checks.each do |binary|
+    describe file("#{File.join(bin_directory, binary)}") do
+      it { should exist }
+      its('size') { should > 0 }
+      it { should be_executable }
+    end
+  end
+
+  version_check_allow_list.each do |binary|
+    describe command("#{File.join(bin_directory, binary)} --version") do
+      its('stdout') { should_not be_empty }
+      its('stderr') { should be_empty }
+      its('exit_status') { should eq 0 }
+    end
   end
 end
